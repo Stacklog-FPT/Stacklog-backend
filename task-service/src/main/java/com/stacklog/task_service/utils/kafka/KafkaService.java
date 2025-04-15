eea00d28e53030e307a5ee0c3a74065dcd77d594
nhat.truong <truongnhat2102@gmail.com>
2025-04-15 17:29:24 +0700

    feat(update redis)

--- task-service/src/main/java/com/stacklog/task_service/utils/kafka/KafkaService.java
+++ task-service/src/main/java/com/stacklog/task_service/utils/kafka/KafkaService.java
@@ -1,24 +1,12 @@
 package com.stacklog.task_service.utils.kafka;
 
-import java.time.LocalDateTime;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.stereotype.Service;
-
-import com.stacklog.task_service.model.entities.Task;
-
-@Service
 public class KafkaService {
 
-    @Autowired
-    KafkaTemplate<String, Object> kafkaTemplate;
+    KafkaProducer kafkaProducer;
+
 
 
     public void sendNotification(String statisticMessage) {
-        Statastic stat = new Statastic(statisticMessage, LocalDateTime.now());
-        kafkaTemplate.send("notification", new Task());
-        kafkaTemplate.send("statistic", stat);
     }
 
 
