502b44743e909556ca5e30705e9594550c5cbd7f
nhat.truong <truongnhat2102@gmail.com>
2025-04-15 15:24:53 +0700

    fix(config all): config dockercompose, gateway, task-service,...

--- auth-service/app.js
+++ auth-service/app.js
@@ -27,5 +27,7 @@ app.get("/", (req, res) => {
     res.send("Auth Service is Running...");
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app;
