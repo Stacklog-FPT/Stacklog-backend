642c5299b011c3d002b0a3946477b8326e23ad43
nhat.truong <truongnhat2102@gmail.com>
2025-04-15 09:47:46 +0700

    feat(add profile-service nodejs)

---
+++ profile-service/app.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/userRoutes');
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.error('MongoDB Connection Error:', err));
+
+app.use('/user', userRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
