17c7058b49c5946c5ce6e215c0146a586e3dfe55
nhat.truong <truongnhat2102@gmail.com>
2025-04-21 16:16:33 +0700

    feat(create notice service)

---
+++ notification-service/src/main/java/com/stacklog/notification_service/model/repo/UserNoticeRepo.java
@@ -0,0 +1,11 @@
+package com.stacklog.notification_service.model.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.stacklog.notification_service.model.entities.UserNotice;
+
+@Repository
+public interface UserNoticeRepo extends JpaRepository<UserNotice, Long> {
+    
+}
