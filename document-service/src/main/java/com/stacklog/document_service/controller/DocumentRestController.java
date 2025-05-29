df086b3d0b1539ea825fbb8bb277b2a739bdf1d5
nhat.truong <truongnhat2102@gmail.com>
2025-05-29 09:59:12 +0700

    update

--- document-service/src/main/java/com/stacklog/document_service/controller/DocumentRestController.java
+++ document-service/src/main/java/com/stacklog/document_service/controller/DocumentRestController.java
@@ -12,13 +12,16 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.stacklog.document_service.model.entities.Document;
 import com.stacklog.document_service.model.service.DocumentService;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,11 +33,13 @@ public class DocumentRestController {
     @Autowired
     DocumentService documentService;
 
-    @PostMapping("/upload")
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<Document> uploadFile(@RequestParam(name = "file") MultipartFile multipartFile,
-            @RequestBody Document e, 
+            @RequestParam("document") String eStr, 
             @RequestHeader("Authorization") String token) {
-        Document document = documentService.saveFile(multipartFile, e, token);
+        
+        Document document = convertStringToDocument(eStr);
+        document = documentService.saveFile(multipartFile, document, token);
         return ResponseEntity.ok().body(document);
     }
 
@@ -53,6 +58,22 @@ public class DocumentRestController {
             e.printStackTrace();
         }
         return ResponseEntity.badRequest().build();
+    }
+
+
+    private Document convertStringToDocument(String eString) {
+        String json = "{\"documentId\":\"\",\"documentTitle\":\"\",\"documentContentType\":\"\",\"documentDownloadUri\":\"\"}";
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            Document doc = objectMapper.readValue(json, Document.class);
+            return doc;
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
