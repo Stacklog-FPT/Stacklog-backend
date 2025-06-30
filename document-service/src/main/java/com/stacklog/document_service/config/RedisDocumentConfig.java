2af5864715d622b2a39ec73077bdbcf39cd5530c
nhat.truong <truongnhat2102@gmail.com>
2025-06-30 22:21:58 +0700

    fix(update cors)

--- document-service/src/main/java/com/stacklog/document_service/config/RedisDocumentConfig.java
+++ document-service/src/main/java/com/stacklog/document_service/config/RedisDocumentConfig.java
@@ -1,7 +1,5 @@
 package com.stacklog.document_service.config;
 
-import java.util.function.Function;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.RedisTemplate;
