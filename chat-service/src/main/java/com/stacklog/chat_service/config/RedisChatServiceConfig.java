78eb2a1e8618639140151c5c52b7a4769a731bcb
nhat.truong <truongnhat2102@gmail.com>
2025-07-11 19:27:44 +0700

    fix(hotfix redis chat-service)

--- chat-service/src/main/java/com/stacklog/chat_service/config/RedisChatServiceConfig.java
+++ chat-service/src/main/java/com/stacklog/chat_service/config/RedisChatServiceConfig.java
@@ -2,6 +2,7 @@ package com.stacklog.chat_service.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -13,6 +14,11 @@ import com.stacklog.core_service.utils.redis.RedisService;
 
 @Configuration
 public class RedisChatServiceConfig {
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory("redis", 6379);
+    }
     
     @Bean
     public RedisService<ChatMessage> redisChatMessageService(
