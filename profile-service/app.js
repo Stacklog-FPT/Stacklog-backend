f703cc7ca24e45345791c6342914ee3a74f6a996
nhat.truong <truongnhat2102@gmail.com>
2025-07-15 23:34:33 +0700

    fix(update role for profile-service)

--- profile-service/app.js
+++ profile-service/app.js
@@ -1,11 +1,17 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
 const userRoutes = require('./routes/userRoutes');
 const { initConsumer } = require('./config/kafka');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
 
 (async() => {
     await initConsumer();
