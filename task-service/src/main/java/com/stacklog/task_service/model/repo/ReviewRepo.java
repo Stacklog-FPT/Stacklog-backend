b62c3ad814a547d4dc64e33653776cfef6fc52b6
nhat.truong <truongnhat2102@gmail.com>
2025-07-26 16:51:11 +0700

    fix(update review-task-service)

--- task-service/src/main/java/com/stacklog/task_service/model/repo/ReviewRepo.java
+++ task-service/src/main/java/com/stacklog/task_service/model/repo/ReviewRepo.java
@@ -3,6 +3,8 @@ package com.stacklog.task_service.model.repo;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.stacklog.task_service.model.entities.Review;
@@ -10,6 +12,7 @@ import com.stacklog.task_service.model.entities.Review;
 @Repository
 public interface ReviewRepo extends JpaRepository<Review, String> {
 
-    List<Review> findByTaskTaskId(String taskId);
+    @Query("SELECT r FROM Review r WHERE r.task.taskId = :taskId")
+List<Review> findByTaskId(@Param("taskId") String taskId);
     
 }
