70ca565ed438e6c83dd67a06db8f077b5a2fc22a
nhat.truong <truongnhat2102@gmail.com>
2025-07-26 20:18:52 +0700

    fix(fix controller saveGroupss)

--- class-service/src/main/java/com/stacklog/class_service/controller/GroupRestController.java
+++ class-service/src/main/java/com/stacklog/class_service/controller/GroupRestController.java
@@ -7,8 +7,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.stacklog.class_service.model.entities.Classes;
+import com.stacklog.class_service.model.entities.GroupStudent;
 import com.stacklog.class_service.model.entities.Groupss;
+import com.stacklog.class_service.model.service.ClassService;
 import com.stacklog.class_service.model.service.GroupService;
+import com.stacklog.class_service.model.service.GroupsStudentService;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,8 +24,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 @RequestMapping(path = "/group")
 public class GroupRestController {
-    
-    @Autowired GroupService groupService;
+
+    @Autowired
+    GroupService groupService;
+
+    @Autowired
+    GroupsStudentService groupsStudentService;
+
+    @Autowired
+    ClassService classService;
 
     @GetMapping("")
     public ResponseEntity<List<Groupss>> getGroupByUserId(@RequestHeader("Authorization") String token) {
@@ -30,23 +41,50 @@ public class GroupRestController {
     }
 
     @GetMapping("/class/{classId}")
-    public ResponseEntity<List<Groupss>> getGroupByClassId(@RequestHeader("Authorization") String token, @PathVariable(name = "classId") String classId) {
+    public ResponseEntity<List<Groupss>> getGroupByClassId(@RequestHeader("Authorization") String token,
+            @PathVariable(name = "classId") String classId) {
         List<Groupss> groupsses = groupService.getAllByClassId(token, classId);
         return ResponseEntity.ok().body(groupsses);
     }
-    
-    
+
     @PostMapping("")
-    public ResponseEntity<Groupss> saveGroupss(@RequestBody Groupss groupss, @RequestHeader("Authorization") String token) {
-        Groupss newGroupss = groupService.save(groupss, token);
-        
+    public ResponseEntity<Groupss> saveGroupss(@RequestBody GroupDTO groupDTO,
+            @RequestHeader("Authorization") String token) {
+        Groupss newGroupss = new Groupss();
+        newGroupss.setGroupsName(groupDTO.groupsName);
+        newGroupss.setGroupsDescriptions(groupDTO.groupsDescriptions);
+        newGroupss.setGroupsMaxMember(groupDTO.groupsMaxMember);
+        newGroupss.setGroupsAvgScore(0.00);
+        newGroupss.setGroupsLeaderId(groupDTO.groupsLeaderId);
+        Classes clazz = classService.getById(groupDTO.classId, token);
+        if (clazz == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        newGroupss.setClasses(clazz);
+        newGroupss = groupService.save(newGroupss, token);
+        for (String userId : groupDTO.groupUserUserIds) {
+            GroupStudent groupStudent = new GroupStudent();
+            groupStudent.setGroups(newGroupss);
+            groupStudent.setUserId(userId);
+            groupsStudentService.save(groupStudent, token);
+        }
+
         return ResponseEntity.ok().body(newGroupss);
     }
 
     @DeleteMapping("/{groupsId}")
-    public ResponseEntity<Groupss> deleteGroupss(@RequestHeader("Authorization") String token, @PathVariable(name = "groupsId") String groupsId) {
+    public ResponseEntity<Groupss> deleteGroupss(@RequestHeader("Authorization") String token,
+            @PathVariable(name = "groupsId") String groupsId) {
         return ResponseEntity.ok().body(null);
     }
-    
 
+}
+
+class GroupDTO {
+    String groupsName;
+    String groupsDescriptions;
+    Integer groupsMaxMember;
+    String groupsLeaderId;
+    String classId;
+    List<String> groupUserUserIds;
 }
