613168ff605eece45a1d8d8eb5c55d0a1c50ddad
nhat.truong <truongnhat2102@gmail.com>
2025-07-26 16:07:53 +0700

    fix(update scheduleservice)

--- schedule-service/src/main/java/com/stacklog/schedule_service/Config/RedisSlotConfig.java
+++ schedule-service/src/main/java/com/stacklog/schedule_service/Config/RedisSlotConfig.java
@@ -2,6 +2,7 @@ package com.stacklog.schedule_service.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -12,6 +13,10 @@ import com.stacklog.schedule_service.model.entities.Slot;
 @Configuration
 public class RedisSlotConfig {
     
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory("redis", 6379);
+    }
 
     @Bean
     public RedisService<Slot> redisSlotService(
