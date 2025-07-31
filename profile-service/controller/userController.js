459b8a9ba83ed0dce098ed2096ac17356c513ea7
nhat.truong <truongnhat2102@gmail.com>
2025-07-31 22:00:08 +0700

    fix(add getById)

--- profile-service/controller/userController.js
+++ profile-service/controller/userController.js
@@ -73,7 +73,7 @@ exports.findByGroupId = async (req, res) => {
 exports.findById = async (req, res) => {
     const { userId } = req.params;
     try {
-        const user = await User.findById({ _id: userId, isDeleted: false });
+        const user = await User.findOne({ _id: userId, isDeleted: false });
         if (user == null) return res.status(404).json({ error: `No ${userId} found` });
         res.status(200).json({ user });
     } catch (error) {
