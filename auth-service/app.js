c4fc34f33abc0dddf483e9c45c3b396c9231ce02
nhat.truong <truongnhat2102@gmail.com>
2025-08-11 23:36:27 +0700

    fix(newest git)

--- auth-service/app.js
+++ auth-service/app.js
@@ -11,7 +11,17 @@ connectDB();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: [
+        'http://localhost:5173',
+        'https://stacklog.io.vn',
+        'https://www.stacklog.io.vn',
+        'https://*.vercel.app'
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}));
 app.use(helmet());
 app.use(morgan("dev"));
 
