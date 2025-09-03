db83fdc1ffdfe0c1573b62bd24a8ff4345644cdd
nhat.truong <truongnhat2102@gmail.com>
2025-09-03 22:32:56 +0700

    hotfix

--- task-service/src/main/java/com/stacklog/task_service/model/repo/TaskAssignRepo.java
+++ task-service/src/main/java/com/stacklog/task_service/model/repo/TaskAssignRepo.java
@@ -3,18 +3,28 @@ package com.stacklog.task_service.model.repo;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.stacklog.task_service.model.entities.TaskAssign;
 
-
 @Repository
 public interface TaskAssignRepo extends JpaRepository<TaskAssign, String> {
-    
+
     public List<TaskAssign> findByAssignTo(String assignTo);
 
     public List<TaskAssign> findByTaskTaskId(String taskId);
 
-    boolean existsByTaskTaskIdAndAssignTo(String taskId, String assignTo);
+    @Query(value = """
+            SELECT EXISTS (
+              SELECT 1
+              FROM task_assign ta
+              WHERE ta.task_id = :taskId
+                AND ta.assign_to = :assignTo
+            )
+            """, nativeQuery = true)
+    boolean existsByTaskIdAndAssignTo(@Param("taskId") String taskId,
+            @Param("assignTo") String assignTo);
 
 }
