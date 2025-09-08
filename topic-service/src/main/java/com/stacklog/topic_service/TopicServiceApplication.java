3af702f9e7cef712c86c1186121c1720be54cc57
nhat.truong <truongnhat2102@gmail.com>
2025-09-08 11:00:20 +0700

    hotfix

--- topic-service/src/main/java/com/stacklog/topic_service/TopicServiceApplication.java
+++ topic-service/src/main/java/com/stacklog/topic_service/TopicServiceApplication.java
@@ -3,7 +3,10 @@ package com.stacklog.topic_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.stacklog.topic_service", "com.stacklog.core_service"})
+@EnableJpaRepositories(basePackages = "com.stacklog.topic_service.model.repo")
+@EntityScan(basePackages = "com.stacklog.topic_service.model.entities")
+@EnableFeignClients
 public class TopicServiceApplication {
 
 	public static void main(String[] args) {
