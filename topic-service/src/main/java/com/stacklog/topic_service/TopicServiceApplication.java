8da3e97a0414a69ec87348a4fe2e94fa8738b992
nhat.truong <truongnhat2102@gmail.com>
2025-09-08 11:02:36 +0700

    feat(topicservice)

--- topic-service/src/main/java/com/stacklog/topic_service/TopicServiceApplication.java
+++ topic-service/src/main/java/com/stacklog/topic_service/TopicServiceApplication.java
@@ -2,6 +2,9 @@ package com.stacklog.topic_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"com.stacklog.topic_service", "com.stacklog.core_service"})
 @EnableJpaRepositories(basePackages = "com.stacklog.topic_service.model.repo")
