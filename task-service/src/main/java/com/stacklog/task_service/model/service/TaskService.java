0e5dcd9c78cd6897a091d807a4702237b0e7998d
nhat.truong <truongnhat2102@gmail.com>
2025-09-11 12:57:47 +0700

    fix delete

--- task-service/src/main/java/com/stacklog/task_service/model/service/TaskService.java
+++ task-service/src/main/java/com/stacklog/task_service/model/service/TaskService.java
@@ -147,7 +147,9 @@ public class TaskService implements IService<Task> {
             e.getAssigns().stream().forEach(a -> taskAssignService.save(a, token));
         }
         if (e.getReviews() != null && !e.getReviews().isEmpty()) {
-            reviewService.deleteReviews(e.getTaskId(), e.getReviews());
+            if (!isCreate) {
+                reviewService.deleteReviews(e.getTaskId(), e.getReviews());
+            }
             e.getReviews().stream().forEach(r -> reviewService.save(r, token));
         }
         if (e.getCheckLists() != null && !e.getCheckLists().isEmpty()) {
