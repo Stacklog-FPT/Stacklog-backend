d567cd062f843f0bbaea1505d53a7050047d168e
nhat.truong <truongnhat2102@gmail.com>
2025-09-18 10:48:51 +0700

    fix(list boxchat)

--- chat-service/models/box.js
+++ chat-service/models/box.js
@@ -3,22 +3,22 @@ const { Schema, model } = require('mongoose');
 const { v4: uuidv4 } = require('uuid');
 
 const MemberSchema = new Schema({
-  isAdmin:{ type: Boolean, default: false },
+  isAdmin: { type: Boolean, default: false },
   isMute: { type: Boolean, default: false },
   joinedAt: { type: Date, default: Date.now },
-  addedBy:  { type: String, default: null },
+  addedBy: { type: String, default: null },
   userId: { type: String, default: null }
 }, { _id: false });
 
 const BoxChatSchema = new Schema({
-  _id:       { type: String, required: true },          // box_chat_id (uuid hoặc groupId)
-  name_box:  { type: String },
-  ava_box:   { type: String },
-  created_by:{ type: String },
-  members:   { type: [MemberSchema], default: [] }
+  _id: { type: String, required: true },          // box_chat_id (uuid hoặc groupId)
+  name_box: { type: String },
+  ava_box: { type: String },
+  created_by: { type: String },
+  members: { type: [MemberSchema], default: [] }
 }, { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }, versionKey: false });
 
-BoxChatSchema.virtual('box_chat_id').get(function(){ return this._id; });
+BoxChatSchema.virtual('box_chat_id').get(function () { return this._id; });
 BoxChatSchema.index({ 'members.user': 1 });
 BoxChatSchema.index({ updated_at: -1 });
 
@@ -42,9 +42,12 @@ async function addMembers(boxId, operatorId, memberIds = []) {
   if (!uniqueIds.length) return;
   await BoxChat.updateOne(
     { _id: boxId },
-    { $addToSet: {
-        members: { $each: uniqueIds.map(uid => ({ userId: uid, isAdmin: false, isMute: false, addedBy: operatorId }))
-      } }
+    {
+      $addToSet: {
+        members: {
+          $each: uniqueIds.map(uid => ({ userId: uid, isAdmin: false, isMute: false, addedBy: operatorId }))
+        }
+      }
     }
   );
 }
@@ -52,7 +55,9 @@ async function addMembers(boxId, operatorId, memberIds = []) {
 async function listBoxesByUser(userId) {
   return BoxChat.find({ 'members.userId': userId })
     .sort({ updated_at: -1 }) // dùng updated_at cho thống nhất
-    .select({ _id: 1, name_box: 1, ava_box: 1, updated_at: 1, members: [] })
+    .select({
+      _id: 1, name_box: 1, ava_box: 1, updated_at: 1, members: 1, created_at: 1, updated_at: 1,
+    })
     .lean();
 }
 
@@ -69,7 +74,8 @@ async function autoCreateBoxFromGroupEvent({ groupId, name, avatar, memberIds = 
 
   await BoxChat.updateOne(
     { _id: groupId },
-    { $addToSet: {
+    {
+      $addToSet: {
         members: { $each: uniqueIds.map(uid => ({ userId: uid, isAdmin: false, isMute: false, addedBy: createdBy })) }
       }
     }
