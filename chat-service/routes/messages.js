b4413cd5a4e8247ce554912e15d552c9d8f301cf
nhat.truong <truongnhat2102@gmail.com>
2025-09-29 15:40:39 +0700

    fix(seen chat)

--- chat-service/routes/messages.js
+++ chat-service/routes/messages.js
@@ -6,6 +6,7 @@ router.post('/:boxId', auth, ctrl.send);
 router.get('/:boxId', auth, ctrl.list);
 router.put('/recall/:messageId', auth, ctrl.recall);
 router.delete('/:messageId', auth, ctrl.remove);
-router.post('/read/:boxId', auth, ctrl.readReset);
+// router.post('/read/:boxId', auth, ctrl.readReset);
+router.post('/read/:boxId', auth, ctrl.markAsRead);
 
 module.exports = router;
