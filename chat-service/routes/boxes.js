9fc365dea60c684d3f7dc29ccef48de9d1ccf52e
nhat.truong <truongnhat2102@gmail.com>
2025-10-21 07:14:22 +0700

    fix/chat

--- chat-service/routes/boxes.js
+++ chat-service/routes/boxes.js
@@ -4,7 +4,7 @@ const auth = require('../middleware/auth');
 
 router.post('/', auth, ctrl.create);
 router.post('/:boxId/members', auth, ctrl.addMembers);
-router.delete('/:boxId/delete/:userId', auth, ctrl.deleteMember);
+router.delete('/:boxId/delete/:memberId', auth, ctrl.deleteMember);
 router.get('/', auth, ctrl.listByUser);
 router.delete('/:boxId', auth, ctrl.delete);
 
