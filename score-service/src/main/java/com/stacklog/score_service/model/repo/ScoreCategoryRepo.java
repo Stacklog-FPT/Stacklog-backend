66a079df71aef3018726a6f2528bd07c4d012c26
nhat.truong <truongnhat2102@gmail.com>
2025-10-22 11:06:35 +0700

    fix/score

--- score-service/src/main/java/com/stacklog/score_service/model/repo/ScoreCategoryRepo.java
+++ score-service/src/main/java/com/stacklog/score_service/model/repo/ScoreCategoryRepo.java
@@ -14,6 +14,7 @@ public interface ScoreCategoryRepo extends JpaRepository<ScoreCategory, String> 
     @Query("select sc from ScoreCategory sc where sc.classId = :classId")
     List<ScoreCategory> findAllByClassId(String classId);
 
+    @Query("select sc from ScoreCategory sc where sc.isReusable = true")
     List<ScoreCategory> findAllByIsReusable(boolean isReuse);
     
 }
