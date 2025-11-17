34f80061ad14869ff30b1b175d17b1d53b8bbad1
nhat.truong <truongnhat2102@gmail.com>
2025-11-17 18:12:37 +0700

    fix/adminclasses

--- class-service/src/main/java/com/stacklog/class_service/model/repo/ClassesRepo.java
+++ class-service/src/main/java/com/stacklog/class_service/model/repo/ClassesRepo.java
@@ -27,7 +27,4 @@ public interface ClassesRepo extends JpaRepository<Classes, String> {
 
   List<Classes> findAllByLectureIdAndSemesterSemesterId(String lectureId, String semesterId);
 
-  @Query("SELECT cl FROM Classes cl WHERE cl.semester.semesterId = :semesterId")
-  List<Classes> findAllBySemesterSemesterId(@Param("semesterId") String semesterId);
-
 }
