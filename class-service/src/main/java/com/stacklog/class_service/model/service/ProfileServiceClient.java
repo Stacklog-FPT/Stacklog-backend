a395142e4c16dc401176c6e6b0cf4d142f3a4223
nhat.truong <truongnhat2102@gmail.com>
2025-11-23 21:07:26 +0700

    fix

--- class-service/src/main/java/com/stacklog/class_service/model/service/ProfileServiceClient.java
+++ class-service/src/main/java/com/stacklog/class_service/model/service/ProfileServiceClient.java
@@ -2,22 +2,23 @@ package com.stacklog.class_service.model.service;
 
 import java.util.List;
 
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-@FeignClient(name = "profile-service", url = "http://profileservice:2001/group", path = "")
-public interface ClassServiceClient {
-    @GetMapping("/find/{semesterId}")
-    List<Groupss> getGroupssBySemesterId(
+@FeignClient(name = "profile-service", url = "http://profileservice:2001/user", path = "")
+public interface ProfileServiceClient {
+    @GetMapping("/class/{classId}")
+    List<Profile> getProfileByClassId(
             @RequestHeader("Authorization") String token,
-            @PathVariable("semesterId") String semesterId);
+            @PathVariable("classId") String classId);
 }
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-class Groupss {
+class Profile {
     private String groupsId; // đổi tên khớp với JSON trả về
 
     public String getGroupsId() { return groupsId; }
