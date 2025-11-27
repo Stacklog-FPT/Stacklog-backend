fbda9c8339cc888ff980a0cf4f44b77e74d1162a
nhat.truong <79240278+truongnhat2102@users.noreply.github.com>
2025-11-27 09:19:57 +0700

    Add LinkedHashMap import to ScoreItemService

--- score-service/src/main/java/com/stacklog/score_service/model/service/ScoreItemService.java
+++ score-service/src/main/java/com/stacklog/score_service/model/service/ScoreItemService.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.stream.Collectors;
+import java.util.LinkedHashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
